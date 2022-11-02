11049f54df5a26a703b7fe5ab6ac38f862e3f45a
TheViralClovers <theviralclovers@gmail.com>
2022-11-02 12:35:46 +0530

    typo favorites

--- solidity_data_types.sol
+++ solidity_data_types.sol
@@ -4,9 +4,9 @@ pragma solidity >=0.7.0 <0.9.0;
 
 contract Storage {
     //different data types demonstration
-    bool hasFavouriteNumber = false; //boolean data types
-    uint256 favouriteNumber = 5; //unsigned integer of size 256 bits (minimum is 8 bits)
-    string favouriteNumberInText = "Five";
+    bool hasFavoriteNumber = false; //boolean data types
+    uint256 favoriteNumber = 5; //unsigned integer of size 256 bits (minimum is 8 bits)
+    string favoriteNumberInText = "Five";
     int256 negNumber = 89; //integers that can hold negative values
     //bit size isnt necessary to be specifeid after the data type, but explicit declaration is good practice
     address myaddress = 0xA9df7Cd5ca3Fe8c0890ba9d00B198d87F04014C7; //stores addresses 
